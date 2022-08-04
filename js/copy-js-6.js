f726a43984d02215a7d1490cf5ff671a4c509f0f
kolorete-ph <110361958+kolorete-ph@users.noreply.github.com>
2022-08-04 19:13:55 +0800

    My First Commit

---
+++ js/copy-js-6.js
@@ -0,0 +1,124 @@
+
+let codecopy1 = document.getElementById("codecopy1");
+let btncopy1 = document.getElementById("btncopy1");
+
+btncopy1.onclick = function() {
+
+$('#btncopy1').click(function() {
+  $('#alert').show();
+});
+
+codecopy1.select();
+
+document.execCommand('Copy');
+btncopy1.innerText = 'Copied Successfully!';
+btncopy2.innerText = 'Copy';
+btncopy3.innerText = 'Copy'; 
+btncopy4.innerText = 'Copy';
+btncopy5.innerText = 'Copy';
+btncopy6.innerText = 'Copy'; 
+
+}
+
+
+let codecopy2 = document.getElementById("codecopy2");
+let btncopy2 = document.getElementById("btncopy2");
+
+btncopy2.onclick = function() {
+
+$('#btncopy2').click(function() {
+  $('#alert').show();
+});
+
+codecopy2.select();
+
+document.execCommand('Copy');
+btncopy1.innerText = 'Copy';
+btncopy2.innerText = 'Copied Successfully!';
+btncopy3.innerText = 'Copy'; 
+btncopy4.innerText = 'Copy';
+btncopy5.innerText = 'Copy';
+btncopy6.innerText = 'Copy'; 
+
+}
+
+
+let codecopy3 = document.getElementById("codecopy3");
+let btncopy3 = document.getElementById("btncopy3");
+
+btncopy3.onclick = function() {
+
+$('#btncopy3').click(function() {
+  $('#alert').show();
+});
+
+codecopy3.select();
+
+document.execCommand('Copy');
+btncopy1.innerText = 'Copy';
+btncopy2.innerText = 'Copy';
+btncopy3.innerText = 'Copied Successfully!';
+btncopy4.innerText = 'Copy';
+btncopy5.innerText = 'Copy';
+btncopy6.innerText = 'Copy'; 
+}
+
+let codecopy4 = document.getElementById("codecopy4");
+let btncopy4 = document.getElementById("btncopy4");
+
+btncopy4.onclick = function() {
+
+$('#btncopy4').click(function() {
+  $('#alert').show();
+});
+
+codecopy4.select();
+
+document.execCommand('Copy');
+btncopy1.innerText = 'Copy';
+btncopy2.innerText = 'Copy';
+btncopy3.innerText = 'Copy'; 
+btncopy4.innerText = 'Copied Successfully!';
+btncopy5.innerText = 'Copy';
+btncopy6.innerText = 'Copy'; 
+}
+
+let codecopy5 = document.getElementById("codecopy5");
+let btncopy5 = document.getElementById("btncopy5");
+
+btncopy5.onclick = function() {
+
+$('#btncopy5').click(function() {
+  $('#alert').show();
+});
+
+codecopy5.select();
+
+document.execCommand('Copy');
+btncopy1.innerText = 'Copy';
+btncopy2.innerText = 'Copy';
+btncopy3.innerText = 'Copy'; 
+btncopy4.innerText = 'Copy';
+btncopy5.innerText = 'Copied Successfully!';
+btncopy6.innerText = 'Copy'; 
+}
+
+let codecopy6 = document.getElementById("codecopy6");
+let btncopy6 = document.getElementById("btncopy6");
+
+btncopy6.onclick = function() {
+
+$('#btncopy6').click(function() {
+  $('#alert').show();
+});
+
+codecopy6.select();
+
+document.execCommand('Copy');
+btncopy1.innerText = 'Copy';
+btncopy2.innerText = 'Copy';
+btncopy3.innerText = 'Copy'; 
+btncopy4.innerText = 'Copy';
+btncopy5.innerText = 'Copy';
+btncopy6.innerText = 'Copied Successfully!'; 
+}
